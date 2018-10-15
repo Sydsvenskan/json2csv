c72b8a281539f1102bc70598932b5cf28c641d8d
Hugo Wetterberg <hugo@wetterberg.nu>
2018-10-15 09:59:15 +0200

    change all examples but one to tests

--- json2csv_test.go
+++ json2csv_test.go
@@ -10,17 +10,7 @@ import (
 	"github.com/Sydsvenskan/json2csv"
 )
 
-func ExampleObject() {
-	json2csv.Convert(strings.NewReader(`{
-  "foo": "bar",
-  "baz": [{"a":"b"}, 1, 1.3]
-}`), os.Stdout)
-	// Output:
-	// baz.0.a,baz.1,baz.2,foo
-	// b,1,1.3,bar
-}
-
-func ExampleArray() {
+func ExampleConvert() {
 	json2csv.Convert(strings.NewReader(`[
 {
   "foo": "bar",
@@ -40,22 +30,50 @@ func ExampleArray() {
 	// ,,,,,"it's JSON, what do you expect?"
 }
 
-func ExampleNumber() {
-	json2csv.Convert(strings.NewReader("42"), os.Stdout)
-	// Output:
-	// number
-	// 42
+func TestConvertObject(t *testing.T) {
+	var w strings.Builder
+	json2csv.Convert(strings.NewReader(`{
+  "foo": "bar",
+  "baz": [{"a":"b"}, 1, 1.3]
+}`), &w)
+
+	expect := `baz.0.a,baz.1,baz.2,foo
+b,1,1.3,bar
+`
+	if w.String() != expect {
+		t.Errorf("Expected %q, got %q\n",
+			expect, w.String())
+	}
 }
 
-func ExampleArrayInArray() {
+func TestNumber(t *testing.T) {
+	var w strings.Builder
+	json2csv.Convert(strings.NewReader(`42`), &w)
+
+	expect := `number
+42
+`
+	if w.String() != expect {
+		t.Errorf("Expected %q, got %q\n",
+			expect, w.String())
+	}
+}
+
+func TestArrayInArray(t *testing.T) {
+	var w strings.Builder
 	json2csv.Convert(strings.NewReader(`[
 ["a", "b", "c"],
 ["one", "two", "three"]
-]`), os.Stdout)
-	// Output:
-	// 0,1,2
-	// a,b,c
-	// one,two,three
+]`), &w)
+
+	expect := `0,1,2
+a,b,c
+one,two,three
+`
+	if w.String() != expect {
+		t.Errorf("Expected %q, got %q\n",
+			expect, w.String())
+	}
 }
 
 func TestBadJSON(t *testing.T) {
